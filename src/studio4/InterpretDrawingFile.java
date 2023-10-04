8d8acad760ff274b525411e08430101adfcd3f09
mercury <mercury@localhost>
2023-10-04 17:18:41 -0500

    hahaha

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,56 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled  = in.nextBoolean(); 
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
 		
+		double x1 = parameterOne;
+		double y1 = parameterTwo;
+		double x2 = parameterThree;
+		double y2 = parameterFour;
+
+		
+		if(shapeType.equals( "rectangle")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if(isFilled) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}	
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if(shapeType.equals( "ellipse")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if(isFilled) {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}	
+			else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		else {
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			double x3 = parameterFive;
+			double y3 = parameterSix;
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			double[] x = {x1, x2, x3};
+			double[] y = {y1, y2, y3};
+			if(isFilled) {
+				StdDraw.filledPolygon(x,y);
+				
+			}	
+			else {
+				StdDraw.polygon(x,y);
+			}
+		}
 	}
 }
